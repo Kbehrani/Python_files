81052f956661b83e1cff1e34389063168384f9fd
Kavisha Behrani <kavishabehrani@Kavishas-MacBook-Air.local>
2022-02-25 05:27:22 -0500

    add

--- Test_file1.py
+++ Test_file1.py
@@ -1,2 +1,9 @@
-for i in range(1,10):
- print(i)
+#print("hello")
+
+import pandas as pd
+# print(pd.__version__)
+#df=pd.read_csv('~/Documents/Python_files/test_file.csv')
+df=pd.read_csv('~/Documents/Python_files/test_file1.csv')
+print(df)
+ 
+
