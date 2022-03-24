40f6f7dbc11c4924bf256708f5f35156c104651d
Kavisha Behrani <kavishabehrani@Kavishas-MacBook-Air.local>
2022-03-24 05:32:29 -0400

    add

--- panda_profiling.py
+++ panda_profiling.py
@@ -19,7 +19,140 @@ data=pd.read_csv('~/Documents/Python_files/Clean_Dataset.csv')
 # data.info()
 # data.describe()
 # data.isnull()
-profile=ProfileReport(data,title="Airline data", html={'style': {'full_width': True}})
-profile
-profile.to_file("Airline analysis.html")
-print("feeling excited - wow! I nailed it, let's do something new")
+# profile=ProfileReport(data,title="Airline data", html={'style': {'full_width': True}})
+# profile
+# profile.to_file("Airline analysis.html")
+# print("feeling excited - wow! I nailed it, let's do something new")
+
+# name=input("enter name:")
+# print(name)
+
+# number=input("enter number: ")
+# number=int(number)
+# print(type(number))
+
+# number=int(input("enter number: "))
+# number=float(input("enter number: "))
+# print(type(number))
+
+
+# print(type(number))
+
+# number1=5
+# print(type(number1))
+# number2=5.5
+# print(type(number2))
+
+#assignment operators
+# x=5
+# results =x//2
+# print(results)
+# #concatenate two strings
+# str1="Hey "
+# str2="Jude"
+# print(str1+str2)
+# x,y=5,6
+
+#problem solving
+
+# distance_km=564.5
+# distance_miles=0.62137*distance_km
+# print(distance_miles)
+
+# number=15
+# print(number<10)
+
+# score=int(input("Enter your score:"))
+# if score>50:
+#     print("You have passed your Exam")
+#     print("Congratulations")
+
+# if score<50:
+#     print("Sorry you have failed your exam")
+
+# if score>50:
+#     print("You have passed your Exam")
+#     print("Congratulations")
+# else:
+#     print("Sorry, you have failed your exam")
+
+
+# while loop
+
+# count=5
+
+# while count<10:
+#     print("Counting is interesting")
+#     count=count+1
+
+# For loop
+
+# sequence in python is collection of things in an order
+# text="Python"
+
+# for chracter in text:
+#     print(chracter)
+
+# languages =["English", "French","German"]
+
+# for language in languages:
+#     print(language)
+
+#python range function
+
+# count=1
+
+# while count<=5:
+#     print(count)
+#     count=count+1
+
+# easier way use range function
+
+# for count in range (1,6):
+#     print(count)
+
+# number=int(input("Enter an integer:"))
+# for count in range(1,11):
+#     # product=number*count
+#     # print(number,"x", count,"=", product)
+#     result=count+1
+#     print(result)
+
+total=0
+
+#looping from 1 to 100
+# for number in range (1,101):
+#     total=total+number
+#     print(total)
+
+
+# while repeats block of code until specified test condition is false
+# for loop repeats the block of code until the last item in a seq is reached
+
+#break and continue in python 
+
+# for item in range (1,6):
+#     print(item)
+#     break
+
+# when we encouter break, loop exits
+
+# for item in range (1,6):
+#     break
+#     print(item)
+
+
+# for item in range (1,6):
+#     if item==3:
+#         break
+#     print(item)
+# print("The end")
+
+#while loop
+
+while True:
+    number=float(input("Enter a number:"))
+    if number<0:
+        break
+    print("You entered:", number)
+   
