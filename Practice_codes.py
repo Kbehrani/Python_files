c0ebd037e136b0e8aeec27ee66feba854162b167
Kavisha Behrani <kavishabehrani@Kavishas-MacBook-Air.local>
2022-03-29 06:09:44 -0400

    add

--- Practice_codes.py
+++ Practice_codes.py
@@ -1,10 +1,17 @@
 # python local and global variables 
 
-message="How are you doing?"
+# message="How are you doing?"
 
-def greet():
-    message="How are you"
-    print("Inside message", message)
+# def greet():
+#     message="How are you"
+#     print("Inside message", message)
 
-greet()
-print("Outside message", message)
+# greet()
+# print("Outside message", message)
+
+# Global and Local variables
+def add_numbers(n1,n2):
+    # result=n1+n2
+    # return result
+output=add_numbers(2,5)
+print(output)
